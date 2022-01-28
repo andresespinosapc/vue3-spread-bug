7a5d7cd33b271eb37b262295d4a84f0c320e437a
Andres Espinosa <andresespinosapc@gmail.com>
2022-01-28 20:59:59 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { watch } from 'vue';
+import { useInfiniteQuery } from 'vue-query';
+
+const { data } = useInfiniteQuery('test', () => {
+  return { test: 1 };
+})
+
+watch(data, newData => {
+  if (newData) {
+    console.log('Original data', newData);
+    console.log('Data with spread', { ...newData });
+  }
+}, { immediate: true });
+</script>
+
+<template></template>
